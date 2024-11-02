0904a466c3eda9897ebd857b4f557446cd9eedf8
Derpius <49565664+Derpius@users.noreply.github.com>
2024-11-02 19:55:20 +0000

    Add boilerplate

---
+++ VPKParser.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+/**
+ * Namespace containing all VPKParser classes and functions.
+ */
+namespace VpkParser {}
+
+#include "src/vpk.hpp"
---
+++ src/vpk.cpp
@@ -0,0 +1,4 @@
+#include "vpk.hpp"
+
+namespace VpkParser {
+}
