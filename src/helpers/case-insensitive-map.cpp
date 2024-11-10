6b2930d0a4b7370fc59e56ced9f6d6595b07a3ef
Derpius <49565664+Derpius@users.noreply.github.com>
2024-11-10 14:11:36 +0000

    Fix case insensitive comparer

--- src/helpers/case-insensitive-map.cpp
+++ src/helpers/case-insensitive-map.cpp
@@ -3,8 +3,10 @@
 
 namespace VpkParser {
   bool CaseInsensitiveComparer::operator()(const std::string& lhs, const std::string& rhs) const noexcept {
-    return std::ranges::equal(lhs, rhs, [](const unsigned char a, const unsigned char b) {
-      return std::tolower(a) == std::tolower(b);
+    auto tmp = std::ranges::lexicographical_compare(lhs, rhs, [](const unsigned char a, const unsigned char b) {
+      return std::tolower(a) < std::tolower(b);
     });
+
+    return tmp;
   }
 }
